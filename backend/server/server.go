515075dd9c7e102c744e15e443ad45ab34bcae32
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2023-01-13 21:07:40 +0900

    update CORS  config

--- backend/server/server.go
+++ backend/server/server.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -9,7 +11,7 @@ func ServerRun() {
 	h := new(Handler)
 	e := echo.New()
 	e.Use(middleware.Logger())
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{AllowOrigins: []string{"https://cps-term-project-b.vercel.app"}, AllowCredentials: true}))
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{AllowOrigins: []string{"https://cps-term-project-b.vercel.app/termProject/search"}, AllowMethods: []string{http.MethodGet}}))
 	e.GET("/health", h.Health)
 	e.GET("/search", h.GetFeature)
 	e.Logger.Fatal(e.Start(":8080"))
