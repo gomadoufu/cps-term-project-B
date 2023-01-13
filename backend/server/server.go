d79c637fb076957d8ab4a2cda69d2af35ad84ab8
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2023-01-13 21:43:54 +0900

    update CORS config

--- backend/server/server.go
+++ backend/server/server.go
@@ -11,7 +11,7 @@ func ServerRun() {
 	h := new(Handler)
 	e := echo.New()
 	e.Use(middleware.Logger())
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{AllowOrigins: []string{"https://cps-term-project-b.vercel.app/termProject/search"}, AllowMethods: []string{http.MethodGet}}))
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{AllowOrigins: []string{"https://cps-term-project-b.vercel.app/termProject/search"}, AllowMethods: []string{http.MethodGet}, AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept}}))
 	e.GET("/health", h.Health)
 	e.GET("/search", h.GetFeature)
 	e.Logger.Fatal(e.Start(":8080"))
