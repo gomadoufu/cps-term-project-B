7f57743b25c6544fffd28116b33841559f05dc8c
elephantMas <elephant.mas@gmail.com>
2023-01-13 10:05:37 +0900

    feat:ルーティング

--- frontend/src/components/searchButton/searchButton.tsx
+++ frontend/src/components/searchButton/searchButton.tsx
@@ -3,6 +3,7 @@ import React, { useCallback } from 'react'
 import type { SongFeature } from '@/types'
 
 import styles from '../../styles/searchPage.module.css'
+import { useRouter } from 'next/router'
 
 type Props = {
   baseUrl: string
@@ -11,13 +12,15 @@ type Props = {
 }
 
 const SearchButton = (props: Props) => {
+  const router = useRouter()
   const onClick = useCallback(() => {
     fetch(props.baseUrl + '/feature' + '?song=' + props.song, { method: 'GET', mode: 'cors' })
       .then((res) => res.json())
       .then((data) => {
         props.setState(JSON.stringify(data))
       })
-  }, [props])
+    router.push('/termProject/result')
+  }, [props, router])
 
   return (
     <div className={styles.button} onClick={onClick}>
