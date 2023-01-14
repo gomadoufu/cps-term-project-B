8016ba5ba1d7d59f15e61060f8acb157596e708b
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2023-01-14 15:24:23 +0900

    fix

--- backend/server/server.go
+++ backend/server/server.go
@@ -9,7 +9,7 @@ func ServerRun() {
 	h := new(Handler)
 	e := echo.New()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{AllowOrigins: []string{"https://cps-term-project-b.vercel.app"}, AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept}}))
-	e.GET("/health", h.Health)
 	e.GET("/search", h.GetFeature)
+	e.GET("/health", h.Health)
 	e.Logger.Fatal(e.Start(":8080"))
 }
