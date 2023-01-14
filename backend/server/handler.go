a9fefd133d99624529042856bbbc9d7188db149a
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2023-01-14 14:33:04 +0900

    update CORS

--- backend/server/handler.go
+++ backend/server/handler.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -12,11 +13,15 @@ type Handler struct {
 	songFeature use_spotify.SongFeature
 }
 
+// Health is a health check endpoint /health
 func (h *Handler) Health(c echo.Context) error {
+	fmt.Println("Health Check request received")
 	return c.JSON(http.StatusOK, use_spotify.SongFeature{SongName: "OK", Artist: []string{"OK"}, ImageURL: "OK", Danceability: 0.0, Energy: 0.0})
 }
 
+// GetFeature is a search endpoint /search
 func (h *Handler) GetFeature(c echo.Context) error {
+	fmt.Println("GetFeature request received")
 	song := c.QueryParam("song")
 	h.songFeature.New(song)
 	return c.JSON(http.StatusOK, h.songFeature)
--- backend/server/server.go
+++ backend/server/server.go
@@ -1,8 +1,6 @@
 package server
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -10,8 +8,7 @@ import (
 func ServerRun() {
 	h := new(Handler)
 	e := echo.New()
-	e.Use(middleware.CORS())
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{AllowOrigins: []string{"*"}, AllowMethods: []string{http.MethodGet}, AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept}}))
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{AllowOrigins: []string{"https://cps-term-project-b.vercel.app"}, AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept}}))
 	e.GET("/health", h.Health)
 	e.GET("/search", h.GetFeature)
 	e.Logger.Fatal(e.Start(":8080"))
