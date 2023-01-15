2993fcb7ee30c4f1114272a1baef3ac493d1a671
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2023-01-15 17:31:25 +0900

    update CORS

--- backend/server/server.go
+++ backend/server/server.go
@@ -10,7 +10,8 @@ import (
 func ServerRun() {
 	h := new(Handler)
 	e := echo.New()
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{AllowOrigins: []string{os.Getenv("CLIENT_URL")}, AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept}}))
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{AllowOrigins: []string{os.Getenv("CLIENT_URL")}, AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept}, AllowCredentials: true,
+		MaxAge: 43200}))
 	e.GET("/search", h.GetFeature)
 	e.GET("/health", h.Health)
 	e.Logger.Fatal(e.Start(":8080"))
